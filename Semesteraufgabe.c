147eef3922242cd466c4ceb28e344bd6ac9c6532
SBester001 <sbester001@re-gister.com>
2019-11-16 23:06:22 +0100

    verbesserte graphische Ausgabe

--- Semesteraufgabe.c
+++ Semesteraufgabe.c
@@ -12,6 +12,18 @@ void print();
 void line_1();
 void line_2();
 
+/*enums*/
+enum { none, left, horizontal, right, top, vertical, bottom };
+enum { water, hit_water, ship, hit_ship, destroyed_ship};
+
+/*structs*/
+struct field {
+        short content;
+        short length; /*0 if no ship*/
+        short number; /*0 if no ship*/
+        short position; /*enum left, horizontal, ... (0 if no ship)*/
+};
+
 /*Globale Variablen*/
 /*
 00 -> Wasser "    "
@@ -23,11 +35,13 @@ void line_2();
 int height = 10;
 int width = 10;
 
-int spieler1[10][10];
-int spieler2[10][10];
+struct field spieler1[10][10];
+struct field spieler2[10][10];
 
 
 
+
+/*---------------------------------------- Methoden ----------------------------------------*/
 int main(void)
 {
         print();
@@ -41,8 +55,15 @@ void set_start_values()
         int j;
         for (i = 0; i < width; ++i) {
                 for (j = 0; j < height; ++j) {
-                        spieler1[i][j] = 0;
-                        spieler2[i][j] = 0;
+                        spieler1[i][j].content = water;
+                        spieler1[i][j].length = 0;
+                        spieler1[i][j].number = 0;
+                        spieler1[i][j].position = none;
+                        
+                        spieler1[i][j].content = water;
+                        spieler1[i][j].length = 0;
+                        spieler1[i][j].number = 0;
+                        spieler1[i][j].position = none;
                 }
         }
         get_start(5, 0);
@@ -70,17 +91,23 @@ void get_start(int length, int number)
         while (!end) {
                 printf("Please enter the starting field of ship with length '%i': ", length);
                 input[0] = getchar();
+                
+                /*Fehlerbehandlung bei zu kurzem Input*/
                 if (input[0] != '\n') {
                         input[1] = getchar();
                 } else {
                         input[1] = ' ';
                 }
                 
+                /*Überschüssigen input löschen*/
                 if (input[0] != '\n' && input[1] != '\n') {
                         while ((getchar()) != '\n');
                 }
+                
                 input[0] = toupper(input[0]);
                 input[1] = toupper(input[1]);
+                
+                /*Input validieren*/
                 ic = (input[0] - '0' >= 0 && input[0] - '0' <= height) && (input[1] - 'A' >= 0 && input[1] - 'A' <= width);
                 ci = (input[0] - 'A' >= 0 && input[0] - 'A' <= width) && (input[1] - '0' >= 0 && input[1] - '0' <= height);
                 end = ic || ci;
@@ -96,13 +123,22 @@ void get_start(int length, int number)
                                         int i;
                                         int not_fit = 0;
                                         for (i = 0; i < length; ++i) {
-                                                not_fit += spieler1[input[0] - 'A' + i][input[1] - '0']; /*nicht alle Felder sind frei*/
+                                                not_fit += spieler1[input[0] - 'A' + i][input[1] - '0'].content; /*nicht alle Felder sind frei*/
                                         }
                                         if (not_fit) {
                                                 ship_not_fit(length, number);
-                                        } else {
+                                        } else { /*Legaler input*/
                                                 for (i = 0; i < length; ++i) {
-                                                        spieler1[input[0] - 'A' + i][input[1] - '0'] = 10 + number;
+                                                        spieler1[input[0] - 'A' + i][input[1] - '0'].content = ship;
+                                                        spieler1[input[0] - 'A' + i][input[1] - '0'].length = length;
+                                                        spieler1[input[0] - 'A' + i][input[1] - '0'].number = number;
+                                                        if (i == 0) {
+                                                                spieler1[input[0] - 'A' + i][input[1] - '0'].position = left;
+                                                        } else if (i == length - 1) {
+                                                                spieler1[input[0] - 'A' + i][input[1] - '0'].position = right;
+                                                        } else {
+                                                                spieler1[input[0] - 'A' + i][input[1] - '0'].position = horizontal;
+                                                        }
                                                 }
                                         }
                                 } else {
@@ -114,13 +150,22 @@ void get_start(int length, int number)
                                         int i;
                                         int not_fit = 0;
                                         for (i = 0; i < length; ++i) {
-                                                not_fit += spieler1[input[0] - 'A'][input[1] - '0' + i]; /*nicht alle Felder sind frei*/
+                                                not_fit += spieler1[input[0] - 'A'][input[1] - '0' + i].content; /*nicht alle Felder sind frei*/
                                         }
                                         if (not_fit) {
                                                 ship_not_fit(length, number);
-                                        } else {
+                                        } else { /*Legaler input*/
                                                 for (i = 0; i < length; ++i) {
-                                                        spieler1[input[0] - 'A'][input[1] - '0' + i] = 10 + number;
+                                                        spieler1[input[0] - 'A'][input[1] - '0' + i].content = ship;
+                                                        spieler1[input[0] - 'A'][input[1] - '0' + i].length = length;
+                                                        spieler1[input[0] - 'A'][input[1] - '0' + i].number = number;
+                                                        if (i == 0) {
+                                                                spieler1[input[0] - 'A'][input[1] - '0' + i].position = top;
+                                                        } else if (i == length - 1) {
+                                                                spieler1[input[0] - 'A'][input[1] - '0' + i].position = bottom;
+                                                        } else {
+                                                                spieler1[input[0] - 'A'][input[1] - '0' + i].position = vertical;
+                                                        }
                                                 }
                                         }
                                 } else {
@@ -158,7 +203,7 @@ char get_orientation(int length)
                         printf("'%c' is no legal input!\n\a", input[0]);
                 }
         }
-        return 'h'; /*Will never be reached, but causes warning*/
+        return 'h'; /*Will never be reached, but causes warning if missing*/
 }
 
 void print()
@@ -190,44 +235,65 @@ void line_2(int row)
         int i;
         printf("%2i", row);
         for (i = 0; i < width; ++i) {
-                switch (spieler1[i][row]) {
-                        case 00: printf("%s", "|    "); break;
-                        case 01: printf("%s", "| () "); break;
-                        case 10: printf("%s", "|===="); break;
-                        case 11: printf("%s", "|===="); break;
-                        case 12: printf("%s", "|===="); break;
-                        case 13: printf("%s", "|===="); break;
-                        case 14: printf("%s", "|===="); break;
-                        case 15: printf("%s", "|===="); break;
-                        case 16: printf("%s", "|===="); break;
-                        case 17: printf("%s", "|===="); break;
-                        case 18: printf("%s", "|===="); break;
-                        case 19: printf("%s", "|===="); break;
-                        case 20: printf("%s", "| >< "); break;
-                        case 21: printf("%s", "| >< "); break;
-                        case 22: printf("%s", "| >< "); break;
-                        case 23: printf("%s", "| >< "); break;
-                        case 24: printf("%s", "| >< "); break;
-                        case 25: printf("%s", "| >< "); break;
-                        case 26: printf("%s", "| >< "); break;
-                        case 27: printf("%s", "| >< "); break;
-                        case 28: printf("%s", "| >< "); break;
-                        case 29: printf("%s", "| >< "); break;
-                        case 30: printf("%s", "|(><)"); break;
-                        case 31: printf("%s", "|(><)"); break;
-                        case 32: printf("%s", "|(><)"); break;
-                        case 33: printf("%s", "|(><)"); break;
-                        case 34: printf("%s", "|(><)"); break;
-                        case 35: printf("%s", "|(><)"); break;
-                        case 36: printf("%s", "|(><)"); break;
-                        case 37: printf("%s", "|(><)"); break;
-                        case 38: printf("%s", "|(><)"); break;
-                        case 39: printf("%s", "|(><)"); break;
+                switch (spieler1[i][row].content) {
+                case water:
+                        printf("%s", "|    "); break;
+                case hit_water:
+                        printf("%s", "| () "); break;
+                case ship:
+                        switch (spieler1[i][row].position) {
+                        case left:
+                                printf("%s", "| <= "); break;
+                        case horizontal:
+                                printf("%s", "| == "); break;
+                        case right:
+                                printf("%s", "| => "); break;
+                        case top:
+                                printf("%s", "| /\\ "); break;
+                        case vertical:
+                                printf("%s", "| || "); break;
+                        case bottom:
+                                printf("%s", "| \\/ "); break;
+                        }
+                        break;
+                case hit_ship:
+                        switch (spieler1[i][row].position) {
+                        case left:
+                                printf("%s", "|#<=#"); break;
+                        case horizontal:
+                                printf("%s", "|#==#"); break;
+                        case right:
+                                printf("%s", "|#=>#"); break;
+                        case top:
+                                printf("%s", "|#/\\#"); break;
+                        case vertical:
+                                printf("%s", "|#||#"); break;
+                        case bottom:
+                                printf("%s", "|#\\/#"); break;
+                        }
+                        break;
+                case destroyed_ship:
+                        switch (spieler1[i][row].position) {
+                        case left:
+                                printf("%s", "|<***"); break;
+                        case horizontal:
+                                printf("%s", "|****"); break;
+                        case right:
+                                printf("%s", "|***>"); break;
+                        case top:
+                                printf("%s", "|*/\\*"); break;
+                        case vertical:
+                                printf("%s", "||**|"); break;
+                        case bottom:
+                                printf("%s", "|*\\/*"); break;
+                        }
+                        break;
+                default:
+                        printf("%s", "| ERR"); /*Should never be reached*/
                 }
         }
         printf("%c\n", '|');
 }
-
 
 /* 
 Schiffe:
